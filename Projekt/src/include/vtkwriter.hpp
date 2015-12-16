039580c2729e522d6f2a02d6e43f75d463997dee
joduen <jonas.duennebacke@gmx.de>
2015-12-16 17:18:51 +0100

    vtkwriter mit Grundfunktionalität. Weitere Funktionen werden (nach Bedarf) später hinzugefügt.

---
+++ Projekt/src/include/vtkwriter.hpp
@@ -0,0 +1,262 @@
+// #################################################################################################
+//     Studienprojekt Modellbildung & Simulation - 2015/16
+// #################################################################################################
+// 	   Header: vtkwriter.hpp
+// ------------------------------------Doxygen-Dokumentation----------------------------------------
+///  \file VtkFileWriter.hpp
+///  \brief
+///  Stellt Klasse fuer die Ausgabe der Ergebnisse in eine .vtk Datei bereit.
+///
+//#################################################################################################
+
+#ifndef __VTKWRITER_HPP_
+#define __VTKWRITER_HPP_
+
+#include "logger.hpp"
+#include "Vector.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+namespace Icarus
+{
+class vtkWriter
+{
+    bool* _cell_data_written_last;
+    bool* _point_data_written_last;
+    std::string _filename;
+    std::string _title;
+    unsigned int _tsteps;
+    int _xdim, _ydim, _zdim;
+    int _cells, _points;
+
+public:
+
+///\brief	Konstruktor
+// -------------------------------------------------------------------------------------------------
+///@param 	_filename       string mit dem Dateinamen OHNE DATEIENDUNG
+///@param   _title          Titel der Datei
+///@param   _xdim           Anzahl der Punkte in x Richtung
+///@param   _ydim           Anzahl der Punkte in y Richtung
+///@param   _zdim           Anzahl der Punkte in z Richtung
+///@param   timesteps       Anzahl der Zeitschritte
+    vtkWriter(std::string filename, std::string title, int xdim, int ydim, int zdim, unsigned int timesteps);
+
+
+///\brief	Destruktor
+    ~vtkWriter();
+
+///\brief	Fuegt skalarwertige Punktdaten zu einem Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	data[]	Array der Quelldaten
+///@param   length  Länge des Arrays
+///@param   timestep Zeitschritt, dem die Daten hinzugefuegt werden
+///@param   name    Name der Daten
+
+    template<typename type>
+    void addPointDataToTimestep(const type data[], const int length, const int timestep, std::string name);
+
+///\brief	Fuegt skalarwertige Punktdaten zu einem Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	data    Vektor mit den zu schreibenden Daten
+///@param   timestep Zeitschritt, dem die Daten hinzugefuegt werden
+///@param   name    Name der Daten
+
+    template<typename type>
+    void addPointDataToTimestep(const FullVector<type>& data,const int timestep, const std::string name);
+
+
+///\brief	Fuegt skalarwertige Zelldaten zu einem Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	data[]	Array der Quelldaten
+///@param   length  Länge des Arrays
+///@param   timestep Zeitschritt, dem die Daten hinzugefügt werden
+///@param   name    Name der Daten
+
+//    template<typename type>
+//    void addCellDataToTimestep(const type* data[], const int length, const int timestep, const std::string name);
+
+
+///\brief	Fuegt skalarwertige Zelldaten zu einem Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	data    Vektor mit den zu schreibenden Daten
+///@param   timestep Zeitschritt, dem die Daten hinzugefuegt werden
+///@param   name    Name der Daten
+
+//    template<typename type>
+//    void addCellDataToTimestep(const FullVector<type>& data, const int timestep, const std::string name);
+
+
+///\brief	Fuegt skalarwertige Punktdaten zu allen Zeitschritten hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	data[]	Array der Quelldaten
+///@param   length  Länge des Arrays
+///@param   name    Name der Daten
+
+//    template<typename type>
+//    void addPointDataToAll(const type data[], int length, std::string name);
+
+
+
+///\brief	Fuegt skalarwertige Punktdaten zu allen Zeitschritten hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	data    Vektor mit den zu schreibenden Daten
+///@param   name    Name der Daten
+
+//   template<typename type>
+//   void addPointDataToAll(const FullVector<type>& data,const std::string name);
+
+
+///\brief	Fuegt skalarwertige Zelldaten zu allen Zeitschritten hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	data[]	Array der Quelldaten
+///@param   length  Länge des Arrays
+///@param   name    Name der Daten
+
+//    template<typename type>
+//   void addCellDataToAll(const type data[], int length, std::string name);
+
+
+///\brief	Fuegt skalarwertige Zelldaten zu allen Zeitschritten hinzu
+///         Die Daten werden in der Datei als float abgespeichert
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	data    Vektor mit den zu schreibenden Daten
+///@param   name    Name der Daten
+
+ //   template<typename type>
+ //   void addCellDataToAll(const FullVector<type>& data, const std::string name);
+
+///\brief	Fuegt vektorwertige Punktdaten zu einem Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	datax[]	Array der x Komponenten
+///@param	datay[]	Array der y Komponenten
+///@param	dataz[]	Array der z Komponenten
+///@param   length  Länge der Arrays
+///@param   timestep Zeitschritt, dem die Daten hinzugefuegt werden
+///@param   name    Name der Daten
+
+ //   template<typename type>
+ //   void addPointVecToTimestep(const type datax[], const type datay[], const type dataz[], const int length, const int timestep, std::string name);
+
+
+///\brief	Fuegt vektorwertige Punktdaten zu einem Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	    Datentyp der Quelldaten
+///@param	datax       Vektor mit den x Komponenten der zu schreibenden  Daten
+///@param	datay       Vektor mit den y Komponenten der zu schreibenden  Daten
+///@param	dataz       Vektor mit den z Komponenten der zu schreibenden  Daten
+///@param   timestep    Zeitschritt, dem die Daten hinzugefuegt werden
+///@param   name        Name der Daten
+
+    template<typename type>
+    void addPointVecToTimestep(const FullVector<type>& datax, const FullVector<type>& datay, const FullVector<type>& dataz, const int timestep, const std::string name);
+
+
+
+///\brief	Fuegt vektorwertige Zelldaten zu einem Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	datax[]	Array der x Komponenten
+///@param	datay[]	Array der y Komponenten
+///@param	dataz[]	Array der z Komponenten
+///@param   length  Länge der Arrays
+///@param   timestep Zeitschritt, dem die Daten hinzugefuegt werden
+///@param   name    Name der Daten
+
+ //   template<typename type>
+ //   void addCellVecToTimestep(const type datax[], const type datay[], const type dataz[], const int length, const int timestep, std::string name);
+
+///\brief	Fuegt vektorwertige Punktdaten zu einem Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	    Datentyp der Quelldaten
+///@param	datax       Vektor mit den x Komponenten der zu schreibenden  Daten
+///@param	datay       Vektor mit den y Komponenten der zu schreibenden  Daten
+///@param	dataz       Vektor mit den z Komponenten der zu schreibenden  Daten
+///@param   timestep    Zeitschritt, dem die Daten hinzugefuegt werden
+///@param   name        Name der Daten
+
+//    template<typename type>
+//    void addCellVecToTimestep(const FullVector<type>& datax, const FullVector<type>& datay, const FullVector<type>& dataz, const int timestep, const std::string name);
+
+
+///\brief	Fuegt vektorwertige Punktdaten zu allen Zeitschritten hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	datax[]	Array der x Komponenten
+///@param	datay[]	Array der y Komponenten
+///@param	dataz[]	Array der z Komponenten
+///@param   length  Länge der Arrays
+///@param   name    Name der Daten
+
+ //   template<typename type>
+ //   void addPointVecToAll(const type datax[], const type datay[], const type dataz[], int length, std::string name);
+
+///\brief	Fuegt vektorwertige Punktdaten zu allen Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	    Datentyp der Quelldaten
+///@param	datax       Vektor mit den x Komponenten der zu schreibenden  Daten
+///@param	datay       Vektor mit den y Komponenten der zu schreibenden  Daten
+///@param	dataz       Vektor mit den z Komponenten der zu schreibenden  Daten
+///@param   name        Name der Daten
+
+ //   template<typename type>
+//    void addPointVecToAll(const FullVector<type>& datax, const FullVector<type>& datay, const FullVector<type>& dataz, const std::string name);
+
+
+
+///\brief	Fuegt vektorwertige Zelldaten zu allen Zeitschritten hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	Datentyp der Quelldaten
+///@param	datax[]	Array der x Komponenten
+///@param	datay[]	Array der y Komponenten
+///@param	dataz[]	Array der z Komponenten
+///@param   length  Länge der Arrays
+///@param   name    Name der Daten
+
+//    template<typename type>
+//    void addCellVecToAll(const type datax[], const type datay[], const type dataz[], int length, std::string name);
+
+
+///\brief	Fuegt vektorwertige Zelldaten zu allen Zeitschritt hinzu.
+///         Die Daten werden in der Datei als float abgespeichert.
+// ------------------------------------------------------------------------------------------------
+///@tparam	type 	    Datentyp der Quelldaten
+///@param	datax       Vektor mit den x Komponenten der zu schreibenden  Daten
+///@param	datay       Vektor mit den y Komponenten der zu schreibenden  Daten
+///@param	dataz       Vektor mit den z Komponenten der zu schreibenden  Daten
+///@param   name        Name der Daten
+
+//    template<typename type>
+//    void addCellVecToAll(const FullVector<type>& datax, const FullVector<type>& datay, const FullVector<type>& dataz, const std::string name);
+
+};
+
+}
+
+#include "vtkwriter.tpp"
+
+#endif // __VTKWRITER_HPP_
---
+++ Projekt/src/include/vtkwriter.tpp
@@ -0,0 +1,169 @@
+
+#ifndef __VTKWRITER_TPP_
+#define __VTKWRITER_TPP_
+
+// nur für intellisense
+#include "vector.hpp"
+
+namespace Icarus
+{
+
+vtkWriter::vtkWriter(std::string filename, std::string title, int xdim, int ydim, int zdim, unsigned int timesteps):
+    _filename(filename),
+    _title(title),
+    _tsteps(timesteps),
+    _xdim(xdim),
+    _ydim(ydim),
+    _zdim(zdim)
+    {
+        _cells=((_xdim-1)*(_ydim-1)*(_zdim-1));
+        _points=(_xdim*_ydim*_zdim);
+        _cell_data_written_last=new bool[_tsteps]();
+        _point_data_written_last=new bool[_tsteps]();
+        std::ofstream file;
+        std::string hfname(_filename);
+        for (int i=0; i<tsteps; ++i)
+        {
+            _point_data_written_last[i]=false;
+            _cell_data_written_last[i]=false;
+            hfname=_filename;
+            hfname.append(".vtk.");
+            hfname.append(std::to_string(i));
+            file.open(hfname.c_str(), ios::out | ios::trunc );
+            if (file.is_open())
+            {
+                file << "# vtk DataFile Version 3.0\n";
+                file << title << std::endl;
+                file << "ASCII\nDATASET STRUCTURED_POINTS\nDIMENSIONS ";
+                file << xdim << " " << ydim << " " << zdim << std::endl;
+                file << "ASPECT_RATIO 1 1 1\nORIGIN 0 0 0\n\n";
+                file.close();
+            }
+            else LOG_ERROR("Unable to open file.");
+        }
+    }
+vtkWriter::~vtkWriter()
+{
+    delete cell_data_written_last;
+    delete point_data_written_last;
+}
+
+template<typename type>
+void vtkWriter::addPointDataToTimestep(const type data[], const int length, const int timestep, std::string name)
+{
+    if (0 > timestep || timestep >= _tsteps) LOG_ERROR("invalid timestep")
+    else
+    {
+        if (length==_points)
+        {
+            std::ofstream file;
+            std::string hfname(_filename);
+            hfname.append(".vtk.");
+            hfname.append(std::to_string(timestep));
+            file.open(hfname.c_str(), ios::out | ios::app);
+            if (file.is_open())
+            {
+                if (point_data_written_last[timestep]==false)
+                {
+                    file << "POINT_DATA " << _points <<std::endl;
+                }
+                file << "SCALARS " << name <<" float"<<std::endl;
+                file << "LOOKUP_TABLE default" << std::endl;
+                for (int i=0; i<_points; ++i)
+                {
+                    file << static_cast<float>(data[i])<<std::endl;
+                }
+                file << std::endl;
+                file.close();
+                point_data_written_last[timestep]=true;
+                cell_data_written_last[timestep]=false;
+            }
+            else LOG_ERROR("Unable to open file.");
+        }
+        else
+        {
+            LOG_ERROR("invalid length of data");
+        }
+    }
+}
+
+template<typename type>
+void vtkWriter::addPointDataToTimestep(const FullVector<type>& data,const int timestep, const std::string name)
+{
+    if (0 > timestep || timestep >= _tsteps) LOG_ERROR("invalid timestep")
+    else
+    {
+        if (data.get_dim()==_points)
+        {
+            std::ofstream file;
+            std::string hfname(_filename);
+            hfname.append(".vtk.");
+            hfname.append(std::to_string(timestep));
+            file.open(hfname.c_str(), ios::out | ios::app);
+            if (file.is_open())
+            {
+                if (point_data_written_last[timestep]==false)
+                {
+                    file << "POINT_DATA " << _points <<std::endl;
+                }
+                file << "SCALARS " << name <<" float"<<std::endl;
+                file << "LOOKUP_TABLE default" << std::endl;
+                for (int i=0; i<_points; ++i)
+                {
+                    file << static_cast<float>(data[i])<<std::endl;
+                }
+                file << std::endl;
+                file.close();
+                point_data_written_last[timestep]=true;
+                cell_data_written_last[timestep]=false;
+            }
+            else LOG_ERROR("Unable to open file.");
+        }
+        else
+        {
+            LOG_ERROR("invalid length of data");
+        }
+    }
+}
+
+template<typename type>
+void vtkWriter::addPointVecToTimestep(const FullVector<type>& datax, const FullVector<type>& datay, const FullVector<type>& dataz, const int timestep, const std::string name)
+{
+    if (0 > timestep || timestep >= _tsteps) LOG_ERROR("invalid timestep")
+    else
+    {
+        if (datax.get_dim()==_points || datax.get_dim()==_points || datax.get_dim()==_points )
+        {
+            std::ofstream file;
+            std::string hfname(_filename);
+            hfname.append(".vtk.");
+            hfname.append(std::to_string(timestep));
+            file.open(hfname.c_str(), ios::out | ios::app);
+            if (file.is_open())
+            {
+                if (point_data_written_last[timestep]==false)
+                {
+                    file << "POINT_DATA " << _points <<std::endl;
+                }
+                file << "SCALARS " << name <<" float"<<std::endl;
+                file << "LOOKUP_TABLE default" << std::endl;
+                for (int i=0; i<_points; ++i)
+                {
+                    file << static_cast<float>datax[i]<<" "<<datay[i]<<" "<<dataz[i]<<std::endl;
+                }
+                file << std::endl;
+                file.close();
+                point_data_written_last[timestep]=true;
+                cell_data_written_last[timestep]=false;
+            }
+            else LOG_ERROR("Unable to open file.");
+        }
+        else
+        {
+            LOG_ERROR("invalid length of data");
+        }
+    }
+
+
+}
+#endif // __VTKWRITER_HPP_
