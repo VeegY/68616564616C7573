ffa16ef51689271951feb59d89147567a31c4761
MaMilc <m.milly@web.de>
2016-02-06 15:38:35 +0000

    Basisauswertungen für die FEM

---
+++ Projekt/src/include/basis_test.cpp
@@ -0,0 +1,9 @@
+#include "basis.hpp"
+
+int main()
+{
+    //Icarus::evaluate_Basis3d(1, 1, 10.0, 10.0, 10.0);
+    //Icarus::evaluate_gradient_Basis3d(1, 1, 10.0, 10.0, 10.0);
+    Icarus::evaluate_Basis2d(1, 1, 1, 10.0, 10.0);
+    return 0;
+}
