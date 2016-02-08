8b2728785a9710685f1ffe6ce482d7b2039a5f3d
VeegY <kevin.hollmann@tu-dortmund.de>
2016-02-08 09:55:59 +0100

    WIP

--- Projekt/src/include/benchmark_help.hpp
+++ Projekt/src/include/benchmark_help.hpp
@@ -89,8 +89,8 @@ bool check_result(float *result, float *datah, int *indicesh, float *fvech, int 
         {
             value += datah[i+dim_local*j] * fvech[indicesh[i+dim_local*j]];
         }
-        float diff = value - result;
-        printf("%d\n",1.0e-10);
+        float diff = value - result[i];
+        printf("%f\n",1.0e-10);
         if (diff > 1.0e-10 || diff < -1.0e-10)
         {
             return false;
