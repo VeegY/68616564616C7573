0cc9f15b8eb4319a20e8beba870ad21fdd5b485b
VeegY <kevin.hollmann@tu-dortmund.de>
2016-02-08 11:20:46 +0100

    WIP

--- Projekt/src/benchmark_ax_cuda.cu
+++ Projekt/src/benchmark_ax_cuda.cu
@@ -57,7 +57,7 @@ void performance(int max_row_length, int dim_local, float time_ku, float time_ou
     printf("Kernel Runtime:\t\t\t%f(ms)\n",time_ku*1000);
     printf("Overall Runtime:\t\t%f(ms)\n",time_ou*1000);
     printf("Bandwith(theoretical):\t\t%f/(14.9)(GB/s)\n",time_ku);
-    printf("Flops(Theoretcical):\t\t%f/(326)(GFLOPS/s)\n", (float)flops/time_ku);
+    printf("Flops(Theoretcical):\t\t%f/(326)(GFLOPS/s)\n", (float)flop/time_ku);
     printf("-----------------------------------------------\n");
     printf("-----------------------------------------------\n");
     printf("                ZERO_COPY\n");
@@ -65,7 +65,7 @@ void performance(int max_row_length, int dim_local, float time_ku, float time_ou
     printf("Kernel Runtime:\t\t\t%f(ms)\n",time_kz*1000);
     printf("Overall Runtime:\t\t%f(ms)\n",time_oz*1000);
     printf("Bandwith(theoretical):\t\t%f/(14.9)(GB/s)\n",time_kz);
-    printf("Flops(Theoretcical):\t\t%f/(326)(GFLOPS/s)\n",(float)flops/time_kz);
+    printf("Flops(Theoretcical):\t\t%f/(326)(GFLOPS/s)\n",(float)flop/time_kz);
     printf("-----------------------------------------------\n");
 
 
