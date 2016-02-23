21d8147a7186f1a4200685849a6e94cdbea24aa0
justusaltmeyer <justus.altmeyer@web.de>
2016-02-23 07:57:44 +0100

    add documentation

--- Projekt/src/quadratur.cpp
+++ Projekt/src/quadratur.cpp
@@ -7,7 +7,7 @@
 namespace Icarus
 {
 
-std::vector<double> get_weight(double c, double d)  //Gibt die Gewichte der Quadratur aus
+std::vector<double> get_weight(double c, double d)  //Gibt die Gewichte der Quadratur als Vektor aus 
 {
     double e(c*c*c);
     double f((c*c)*d);
@@ -15,7 +15,7 @@ std::vector<double> get_weight(double c, double d)  //Gibt die Gewichte der Quad
     return {e, f, e, f, g, f, e, f, e, f, g, f, g, d*d*d, g, f, g, f, e, f, e, f, g, f, e, f, e};
 }
 
-void transformation(std::vector<double>& ai, double h, std::vector<double>& trans) //Berechnet 1/2*h*ax[i]+1/2*h zur Transformation auf das Intervall der Länge h
+void transformation(std::vector<double>& ai, double h, std::vector<double>& trans) //Berechnet Transformation auf das Intervall der Länge h
 {
     for(int i=0; i<27; ++i)
     {
@@ -29,10 +29,10 @@ std::vector<double> get_quadrature_xpoints(int e, double h, std::vector<double>&
     //e_x=getXcoordinate(e);
     std::vector<double> x_local{0, 0, h, h, 0, 0, h, h, 0, 0.5*h, h, 0.5*h, 0, 0,
         h, h, 0, 0.5*h, h, 0.5*h, 0.5*h, 0, 0.5*h, h, 0.5*h, 0.5*h, 0.5*h};
-    transformation(ax, h, trans);
+    transformation(ax, h, trans); //Transformation auf das Intervall mit der Länge h
     for(int j=0; j<27; ++j)
     {
-        x_global[j]=trans[j]+1.0+x_local[j];
+        x_global[j]=trans[j]+x_local[j]; //Hier fehlt noch das Einbinden von getXcoordinate(e)!!!
     }
     return x_global;
 }
@@ -43,10 +43,10 @@ std::vector<double> get_quadrature_ypoints(int e, double h, std::vector<double>&
     //e_y=getYcoordinate(e);
     std::vector<double> y_local{h, 0, 0, h, h, 0, 0, h, 0.5*h, h, 0.5*h, h, h,
         0, 0, h, 0.5*h, 0, 0.5*h, h, 0.5*h, 0.5*h, 0, 0.5*h, h, 0.5*h, 0.5*h};
-    transformation(ay, h, trans);
+    transformation(ay, h, trans); //Transformation auf das Intervall mit der Länge h
     for(int l=0; l<27; ++l)
     {
-        y_global[l]=trans[l]+1.0+y_local[l];
+        y_global[l]=trans[l]+1.0+y_local[l];//Hier fehlt noch das Einbinden von getYcoordinate(e)
     }
     return y_global;
 }
@@ -59,15 +59,51 @@ std::vector<double> get_quadrature_zpoints(int e, double h, std::vector<double>&
     //e_z=getZcoordinate(e);
     std::vector<double> z_local{0, 0, 0, 0, h, h, h, h, 0, 0, 0, 0,
         0.5*h, 0.5*h, 0.5*h, 0.5*h, h, h, h, h, 0,  0.5*h, 0.5*h, 0.5*h, 0.5*h, h,  0.5*h};
-    transformation(az, h, trans);
+    transformation(az, h, trans); //Transformation auf das Intervall mit der Länge h
     for(int r=0; r<27; ++r)
     {
-        z_global[r]=trans[r]+1.0+z_local[r];
+        z_global[r]=trans[r]+1.0+z_local[r]; //Hier fehlt noch das Einbinden von getZcoordinate(e)
     }
     return z_global;
 }
 
 /*
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Beispielhafte main-Funktion zum Verständnis, die einzelnen Funktionen benutzt/initialisiert werden müssen: 
+
+int main()
+{
+    double c(5.0/9.0);
+    double d(8.0/9.0);
+    std::vector<double> weight{(get_weight(c, d))}; //Vektor der Gewichte kann jetzt mit "weight" benutzt werden
+    
+    
+    double h(0.01); // Beispielhafte Schrittweite
+
+    double a(sqrt(0.6));
+    std::vector<double> ax{-a, -a, -a, -a, -a, -a, -a, -a, -a, 0, 0, 0, 0, 0, 0, 0, 0, 0, a, a, a, a, a, a, a, a, a};  //x-Koordinaten der Gauss-Quadraturpunkte auf [-1,1]
+    std::vector<double> ay{-a, -a, -a, 0.5, 0, 0, a, a, a, -a, -a, -a, 0, 0, 0, a, a, a, -a, -a, -a, 0, 0, 0.25, a, a, a}; //y-Koordinaten der Gauss-Quadraturpunkte auf [-1,1]
+    std::vector<double> az{-a, 0, a, -a, 0, a, -a, 0, a, -a, 0, a, -a, 0, a, -a, 0, a, -a, 0, a, -a, 0, a, -a, 0, a};   //z-Koordinaten der Gauss-Quadraturpunkte auf [-1,1]
+    
+    
+    int e(1); //Beispielhaftes e 
+    
+    std::vector<double> ai(27);//Wird benutzt in der Funktion transformation
+    std::vector<double> trans(27);//Wird bei der Funktion transformation als Vektor ausgegeben
+    
+    std::vector<double> x_global(27);//X-Koordinaten der entgültigen Quadraturpunkte
+    get_quadrature_xpoints(e, h, ax, trans);
+    std::vector<double> y_global(27);//Y-Koordinaten der entgültigen Quadraturpunkte
+    get_quadrature_ypoints(e, h, ay, trans);
+    std::vector<double> z_global(27); //Z-Koordinaten der entgültigen Quadraturpunkte
+    get_quadrature_zpoints(e, h, az, trans);
+    
+    return 0;
+}
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
 std::vector<double> get_weight(double c, double d)
 {
     return std::vector<double>{5.0};
