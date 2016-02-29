56616b8691e7ea15852f116f5c07e7949baa1f8b
justusaltmeyer <justus.altmeyer@web.de>
2016-02-29 12:37:33 +0100

    Update getxyz.hpp

--- Projekt/src/include/getxyz.hpp
+++ Projekt/src/include/getxyz.hpp
@@ -6,31 +6,27 @@
 namespace Icarus
 {
 
-
-    int getx(size_t index,
-		size_t nx, size_t ny)
+int getx(size_t index, size_t nx, size_t ny)
 	{
 	int	az = (index / nx) / ny;
 	int	ay = index / nx - az*ny;
 	int	ax = index - ay*nx - az*nx*ny;
 
-    return ax;
+    	return ax;
 	}
 
-	int gety(size_t index,
-		size_t nx, size_t ny)
+int gety(size_t index, size_t nx, size_t ny)
 	{
 	int	az = (index / nx) / ny;
 	int	ay = index / nx - az*ny;
 
-    return ay;
+    	return ay;
 	}
 
-	int getz(size_t index,
-		size_t nx, size_t ny)
+int getz(size_t index, size_t nx, size_t ny)
 	{
-    int az = (index / nx) / ny;
+    	int az = (index / nx) / ny;
 
-    return az;
+    	return az;
 	}
 }
