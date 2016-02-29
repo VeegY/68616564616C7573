c3a06407b6bc377465c54d3fcc033431c6b90585
MaMilc <m.milly@web.de>
2016-02-29 13:32:07 +0000

    improved version (Rückgabewerte als Koordinaten)

--- Projekt/src/include/getxyz.hpp
+++ Projekt/src/include/getxyz.hpp
@@ -6,53 +6,31 @@
 namespace Icarus
 {
 
-<<<<<<< HEAD
 
     double getx(size_t index,
 		double h, size_t nx, size_t ny)
-=======
-int getx(size_t index, size_t nx, size_t ny)
->>>>>>> 56616b8691e7ea15852f116f5c07e7949baa1f8b
 	{
 	int	az = (index / nx) / ny;
 	int	ay = index / nx - az*ny;
 	int	ax = index - ay*nx - az*nx*ny;
 
-<<<<<<< HEAD
     return ((double) ax)*h;
 	}
 
 	double gety(size_t index,
 		double h, size_t nx, size_t ny)
-=======
-    	return ax;
-	}
-
-int gety(size_t index, size_t nx, size_t ny)
->>>>>>> 56616b8691e7ea15852f116f5c07e7949baa1f8b
 	{
 	int	az = (index / nx) / ny;
 	int	ay = index / nx - az*ny;
 
-<<<<<<< HEAD
     return ((double)ay)*h;
 	}
 
 	double getz(size_t index,
 		double h, size_t nx, size_t ny)
-=======
-    	return ay;
-	}
-
-int getz(size_t index, size_t nx, size_t ny)
->>>>>>> 56616b8691e7ea15852f116f5c07e7949baa1f8b
 	{
-    	int az = (index / nx) / ny;
+    int az = (index / nx) / ny;
 
-<<<<<<< HEAD
     return ((double) az)*h;
-=======
-    	return az;
->>>>>>> 56616b8691e7ea15852f116f5c07e7949baa1f8b
 	}
 }
