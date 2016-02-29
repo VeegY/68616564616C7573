3becaa9d062cca87bccaefc812a0b0721dc9bd32
joduen <jonas.duennebacke@tu-dortmund.de>
2016-02-29 14:39:08 +0100

    adding mpi tests

---
+++ Projekt/tests/neumann_mpitest.cpp
@@ -0,0 +1,51 @@
+#include "../src/include/slicedvector.hpp"
+#include "../src/include/bicgstabsolver.hpp"
+#include "../src/include/distellpackmatrix.hpp"
+
+#include "../src/include/vtkwriter.hpp"
+#include "../src/include/discretizer.hpp"
+#include "../src/include/assemble.hpp"
+
+double bdry(int vtx_global)
+{
+	if (vtx_global < 10) return 100;
+	return 0.0;
+}
+
+int neumann_demo()
+{
+	const int nx = 10, ny = 10, nz = 10;
+	const float h = 0.1;
+	// diskretisieren
+	//std::vector<char> disc = Icarus::discretizer("leer.obj", h, Nx, Ny, Nz);
+	
+	// assemblieren
+	auto lgs = Icarus::assemble_neumann<double>(nx, ny, nz, h, bdry);
+	//ausgabe der Matrix:
+	std::cout << "Die Matrix A:" << std::endl;
+	Icarus::print_sliced_object(lgs.first);
+	
+	// l�sen
+	size_t n = lgs.first.get_dim_global();
+	Icarus::SlicedVector<double> sol(n);
+	sol.clear();
+	Icarus::BiCgStabSolver<Icarus::DistEllpackMatrix<double>> solver(lgs.first, lgs.second);
+	solver.solve(sol);
+
+	// speichern (nur master)
+	Icarus::FullVector<double> fullsol(sol);	
+	int myrank;
+	MPI_SCALL(MPI_Comm_rank(MPI_COMM_WORLD, &myrank));
+	if (myrank == 0)
+	{
+		Icarus::vtkWriter writer("out", "Testdatensatz", nx, ny, nz, 1);
+		writer.addPointDataToTimestep(fullsol, 0, "Geschwindigkeitspotential");
+	}
+	MPI_SCALL(MPI_Barrier(MPI_COMM_WORLD));
+	return 0;
+}
+
+int main()
+{
+	return neumann_demo();
+}
