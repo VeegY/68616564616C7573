b6d0a1581785b2ebc12aa5e02ae2e57399fbcf8b
Veegy <kevin.hollmann@tu-dortmund.de>
2016-03-07 13:41:48 +0000

    improve style

--- Projekt/src/getxyz.cpp
+++ Projekt/src/getxyz.cpp
@@ -1,36 +1,27 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <iomanip>
-#include <cmath>
-
 #include "include/assemblefem.hpp"
 
 namespace Icarus
 {
 
-
-    double assembleFem::getx(size_t index)
-	{
-	int	az = (index / Nx) / Ny;
-	int	ay = index / Nx - az*Ny;
-	int	ax = index - ay*Nx - az*Nx*Ny;
-
-    return ((double) ax)*h;
-	}
-
-	double assembleFem::gety(size_t index)
-	{
-	int	az = (index / Nx) / Ny;
-	int	ay = index / Nx - az*Ny;
-
-    return ((double)ay)*h;
-	}
-
-	double assembleFem::getz(size_t index)
-	{
+double assembleFem::getx(size_t index)
+{
     int az = (index / Nx) / Ny;
-
-    return ((double) az)*h;
-	}
+    int ay = index / Nx - az*Ny;
+    int ax = index - ay*Nx - az*Nx*Ny;
+    return ((double) ax)*h;
 }
+
+double assembleFem::gety(size_t index)
+{
+    int az = (index / Nx) / Ny;
+    int ay = index / Nx - az*Ny;
+    return ((double)ay)*h;
+}
+
+double assembleFem::getz(size_t index)
+{
+    int az = (index / Nx) / Ny;
+    return ((double) az)*h;
+}
+
+}//namespace Icarus
