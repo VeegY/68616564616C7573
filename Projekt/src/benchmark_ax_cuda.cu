de00a7edfde6ef77ccbae4f80ee6d12a5281bc14
joduen <jonas.duennebacke@tu-dortmund.de>
2016-03-15 16:29:40 +0100

    adding gpu accelerated classes

---
+++ Projekt/src/benchmark_ax_cuda.cu
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <string.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cmath>
+#include <string>
+//#include "include/timer.hpp"
+
+
+template <typename Scalar>
+void cleanup(Scalar *data, Scalar *fvec, Scalar *result, int *indices, int method);
+
+//KERNEL
+template<typename mtype, typename vtype, typename rtype>
+__global__ void  gpu_ax(mtype* data, const vtype* fvec, rtype* result, size_t* indices, size_t max_row_length, size_t dim_local)
+{
+
+    int idx = threadIdx.x+blockDim.x*blockIdx.x;
+    if(idx<dim_local)
+    {
+      int col;
+      rtype svalue = 0;
+      mtype value;
+      for(int i = 0;i < max_row_length; i++)
+      {
+        value = data[i*dim_local+idx];
+        col = indices[i*dim_local+idx];
+        svalue += value*fvec[col];
+      }
+      result[idx]=svalue;
+    }
+}
+
+
+
+//=============================================================================
+//                          KERNEL für DistEllpackKlasse
+//=============================================================================
+template<typename mtype, typename vtype, typename rtype>
+void gpu_ax_(mtype* data, const vtype* fvec, rtype* result, size_t *indices, size_t max_row_length,
+		  size_t dim_local)
+{
+    int num_blocks = ceil((double)dim_local / 1024);
+    int num_threads = ceil(((double)dim_local / num_blocks) / 32) * 32;
+
+            //=================================//
+                gpu_ax << <num_blocks, num_threads >> >(data, fvec, result, indices, max_row_length, dim_local);
+            cudaDeviceSynchronize();
+            //=================================//
+}
+template void gpu_ax_<float, float, float>(float*, const float*, float*, size_t*, size_t, size_t);
+template void gpu_ax_<double, double, double>(double*, const double*, double*, size_t*, size_t, size_t);
+
+
+
+//=============================================================================
+//                              CLEANUP FUNCTIONS
+//=============================================================================
+template <typename Scalar>
+void cleanup(Scalar *data, Scalar *fvec, Scalar *result, int *indices, int method)
+{
+    switch(method)
+    {
+        case(0):
+            cudaFree(data);
+            cudaFree(fvec);
+            cudaFree(result);
+            cudaFree(indices);
+            break;
+        case(1):
+            cudaFreeHost(data);
+            cudaFreeHost(fvec);
+            cudaFreeHost(result);
+            cudaFreeHost(indices);
+            break;
+        case(2):
+            delete[] data;
+            delete[] fvec;
+            delete[] result;
+            delete[] indices;
+            break;
+    }
+}
+template void cleanup<int>(int *data, int *fvec, int *result, int *indices, int method);
+template void cleanup<float>(float *data, float *fvec, float *result, int *indices, int method);
+template void cleanup<double>(double *data, double *fvec, double *result, int *indices, int method);
+
+
+
+
+
+
+
+
+//====Ich war nicht mutig genug es zu loeschen :D===/
+
+template <typename Scalar>
+void cleanupgpu(Scalar *data)
+{
+cudaFree(data);
+}
+template void cleanupgpu<int>(int *data);
+template void cleanupgpu<float>(float *data);
+template void cleanupgpu<double>(double *data);
+template void cleanupgpu<size_t>(size_t *data);
+
+
+//ALLOCATE MEMORY FUNCTION FOR UNIFIED MEMORY for DistEllpack
+template<typename Scalar>
+void alloc_unifiedD(Scalar **data, size_t **indices, int max_row_length, int dim_local)
+{
+cudaMallocManaged((void **)data, sizeof(Scalar)*dim_local*max_row_length);
+cudaMallocManaged((void **)indices, sizeof(size_t)*dim_local*max_row_length);
+}
+template void alloc_unifiedD<int>(int **data, size_t **indices, int max_row_length, int dim_local);
+template void alloc_unifiedD<float>(float **data, size_t **indices, int max_row_length, int dim_local);
+template void alloc_unifiedD<double>(double **data, size_t **indices, int max_row_length, int dim_local);
+
+
+// ALLOCATE MEMORY FUNCTION FOR UNIFIED MEMORY FOR SLICEDVECTOR
+template<typename Scalar>
+void alloc_unifiedV(Scalar **fvec, int dim_fvec)
+{
+cudaMallocManaged((void **)fvec, sizeof(Scalar)*dim_fvec);
+}
+template void alloc_unifiedV<int>(int **fvec, int dim_fvec);
+template void alloc_unifiedV<float>(float **fvec, int dim_fvec);
+template void alloc_unifiedV<double>(double **fvec, int dim_fvec);
---
+++ Projekt/src/include/distellpackmatrixgpu.hpp
@@ -0,0 +1,216 @@
+#ifndef __DISTELLPACKMATRIXGPU_HPP_
+#define __DISTELLPACKMATRIXGPU_HPP_
+
+#include "matrix.hpp"
+#include "utility.hpp"
+#include "mpihandler.hpp"
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+
+
+namespace Icarus
+{
+
+/**
+  * \brief  Dünnbesetzte, quadratische Matrix, deren Zeilen gleichverteilt auf einer Menge von Nodes liegen.
+  *
+  * Die Zeilen dieser Matrix liegen (annähernd) gleichverteilt auf einer Menge
+  * von Nodes der zugeordneten Prozessgruppe. Lokal auf der Node werden die Zeilen
+  * im Ellpack-Format gespeichert für maximale Effizienz der MV-Multiplikation
+  * in CUDA.
+  *
+  * Dieser Matrixtyp kann nur sequentiell zeilenweise gefüllt werden, wobei
+  * die maximale Zeilenlänge (node-weise) bekannt sein muss. Siehe dazu auch
+  * die Dokumtation der Funktion sequential_fill.
+  *
+  * \tparam   Scalar  Skalarer Typ der Einträge.
+  */
+template <typename Scalar>
+class DistEllpackMatrixGpu : public Matrix<DistEllpackMatrixGpu<Scalar>>
+{
+    friend class Matrix<DistEllpackMatrixGpu<Scalar>>;
+
+	// MPI Eigenschaften
+	MPI_Comm _my_comm;
+	int _my_rank, _num_nodes;
+
+    // Mit PAD wird das padding durchgeführt
+    static const int PAD = 0;
+
+    size_t _dim_global, _dim_local, _dim_local_nopad, _max_row_length;
+
+    size_t * _indices;
+
+    Scalar* _data;
+
+    // hilfsvariablen zum sequentiellen füllen der matrix
+    size_t _row_ptr, _col_ptr;
+    bool _filled;
+
+public:
+
+    /// Zugeordneter (d.h. bezüglich der Operatoren vertäglicher) Vektor-Typ.
+    typedef typename MatrixTraits<DistEllpackMatrixGpu<Scalar>>::VectorType VectorType;
+
+    /**
+     * \brief   Standardkonstruktor.
+     *
+     * Erzeugt einen Vektor der Dimension dim, der komplett auf jeder Node der
+     * Prozessgruppe my_comm liegt.
+     *
+     * \param   dim_globaö     Dimension der Matrix.
+     * \param   my_comm         Kommunikator in die Prozessgruppe der Matrix.
+     */
+    DistEllpackMatrixGpu(size_t dim_global, MPI_Comm my_comm = MPI_COMM_WORLD);
+
+    ~DistEllpackMatrixGpu();
+
+	DistEllpackMatrixGpu(DistEllpackMatrixGpu&& other);
+
+	DistEllpackMatrixGpu(const DistEllpackMatrixGpu& other);
+
+	DistEllpackMatrixGpu& operator=(DistEllpackMatrixGpu&& other);
+
+	DistEllpackMatrixGpu& operator=(const DistEllpackMatrixGpu& other);
+
+     /**
+     * \brief   Gibt den Kommunikator in die Prozessgruppe der Matrix zurück.
+     */
+	MPI_Comm get_comm() const { return _my_comm; }
+
+    /**
+      * \brief   Gibt die lokale Dimension der Matrix, d.h. die Anzahl
+      *          der auf der aufrufenden Node gespeicherten Zeilen zurück.
+      */
+    size_t get_dim_local() const { return _dim_local; }
+
+    /**
+      * \brief   Gibt die Anzahl der Zeilen, wie sie auf einer der ersten N-1 Nodes liegen, zurück.
+      */
+    size_t get_dim_local_nopad() const { return _dim_local_nopad; }
+
+    /**
+      * \brief   Gibt die globale Dimension, d.h. die Anzahl der Zeilen der Matrix, zurück.
+      */
+    size_t get_dim_global() const { return _dim_global; }
+
+    //TODO: get, set local/global
+
+    /**
+      * \brief   Bereitet den zeilenweisen Füllvorgang der Matrix vor.
+      *
+      * Diese Funktion muss von jeder Node genau einmal zu Beginn des Füllvorgangs aufgerufen werden.
+      * Anschließend können die Zeilen mit sequential_fill und end_of_row gefüllt werden, beginnend
+      * bei der lokal ersten Zeile.
+      * Die maximale auf dieser Node auftretende Zeilenlänge muss vorher bekannt sein.
+      *
+      * \param max_row_length Maximal auf dieser Node auftretende Zeilenlänge.
+      */
+    void prepare_sequential_fill(size_t max_row_length);
+
+    /**
+      * \brief   Fülle die Matrix zeilenweise.
+      *
+      * Diese Funktion fügt der aktuellen Zeile den Wert val mit Spaltenindex col hinzu.
+      * Vor dem erste Aufruf dieser Funktion muss mit prepare_sequential_fill die maximal
+      * auf der füllenden Node auftretende Zeilenlänge gesetzt werden.
+      * Nachdem der letzte Eintrag einer Zeile gesetzt wurde, wird mit end_of_row
+      * die Zeile beendet.
+      *
+      * \param colind Spaltenindex des einzutregenden Werts. Es muss colind < dim_global gelten.
+      * \param val    Wert, der an die Position colind geschrieben werden soll.
+      */
+    void sequential_fill(size_t colind, const Scalar& val);
+
+    /**
+      * \brief   Beende eine Zeile beim zeilenweisen Füllen der Matrix.
+      *
+      * Diese Funktion beendet die aktuelle Zeile beim Füllvorgang und setzt den
+      * Füllcursor auf die nächste Zeile, oder beendet den Füllvorgang, falls die
+      * letzte auf der Node vorhandene Zeile beendet wurde.
+      *
+      * Vor dem erste Aufruf dieser Funktion muss mit prepare_sequential_fill die maximal
+      * auf der füllenden Node auftretende Zeilenlänge gesetzt werden.
+      *
+      * Diese Funktion muss während eines Füllvorgangs auf der Node genau dim_local mal
+      * aufgerufen werden.
+      */
+    void end_of_row();
+
+    /**
+      * \brief   Prüft, ob die Matrix korrekt gefüllt wurde.
+      *
+      * Ein positiver Rückgabewert dieser Funktion ist einerseits ein Indikator für einen erfolgreich
+      * abgeschlossenen Füllvorgang und andererseits die Voraussetzung für sämtliche algebraische
+      * Operationen mit der Matrix.
+      *
+      * \return Gibt zurück, ob die Matrix korrekt gefüllt wurde.
+      */
+    bool is_filled() const { return _filled; }
+
+    /**
+      * \brief   Gibt den globalen Index der ersten auf der Node liegenden Zeile zurück.
+      */
+    size_t first_row_on_node() const { return _my_rank * _dim_local_nopad; }
+
+    /**
+      * \brief   Erstellt einen zu der Matrix passenden Äquilibrierungsvorkonditionierer.
+      *
+      * \return  Der Vorkonditionierer hat denselben Typ wie das Objekt, auf das die Funktion
+      *          aufgerufen wird.
+      */
+    DistEllpackMatrixGpu precond_equi() const;
+
+    /**
+      * \brief   Erstellt einen zu der Matrix passenden Äquilibrierungsvorkonditionierer.
+      *
+      * Wenn in einer Zeile eine Null auf der Diagonalen steht, wird diese Zeile
+      * durch die Vorkonditionierung nicht verändert.
+      *
+      * \return  Der Vorkonditionierer hat denselben Typ wie das Objekt, auf das die Funktion
+      *          aufgerufen wird.
+      */
+    DistEllpackMatrixGpu precond_jacobi() const;
+
+    /**
+      * \brief   Schreibe den lokalen Inhalt des Block in den Stream out.
+      *
+      * Für die Verwendung dieser Funktion muss eine entsprechende Überladung des
+      * Operators std::ostream::operator<<(Scalar) existieren.
+      *
+      * \param out  Stream, in den die Ausgabe geschrieben werden soll.
+      */
+    void print_local_data(std::ostream &os) const;
+
+     /**
+      * \brief   Lese eine DistEllpackMatrixGpu aus einem CSR-artigen Dateiformat ein.
+      *
+      * Das benötigte Dateiformat wird von dem MATLAB-Skript /util/csrwrite.m
+      * erzeugt. Die Informationen werden in drei Dateien gespeichert, deren Namen
+      * aus einem gemeinsamen Präfix und verschiedenen Endungen bestehen.
+      * Dieser Funktion wird (wie auch csrwrite.m) dieser Präfix übergeben.
+      *
+      * \param filename  Präfix des Dateinamens des Dateitripels, das eingelesen werden soll.
+      * \param new_comm  Kommunikator in die Prozessgruppe, der die neu erzeugte Matrix
+      *                  gehören soll.
+      *
+      * \return Gibt die aus dem Dateitripel erzeugte DistEllpackMatrixGpu zurück.
+      */
+    static DistEllpackMatrixGpu import_csr_file(const std::string& filename, MPI_Comm new_comm = MPI_COMM_WORLD);
+
+private:
+
+    size_t get_dim_impl() const {return _dim_global;}
+
+    void mult_vec_impl(const VectorType& vec, VectorType& result) const;
+};
+
+}
+
+#include "distellpackmatrixgpu.tpp"
+
+
+
+
+#endif // __DISTELLPACKMATRIXGPU_HPP_
