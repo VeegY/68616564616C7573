a6b84cee0a8d22ad7ba929fa712e7f11b647a08f
Veegy <kevin.hollmann@tu-dortmund.de>
2016-03-18 18:30:10 +0000

    add testcase 2 neumann function

--- Projekt/src/mathfunction.cpp
+++ Projekt/src/mathfunction.cpp
@@ -11,50 +11,49 @@ double mathfunction::eval(double x, double y, double z)
     // 1: u, 2: f, 3: dirichlet, 4: neumann
     case 1: return (0.5-x)*(0.5-y)*(0.5-z);
     case 2: return 0.0;
-    case 3: if (x < 1.0e-5 && x > -1e-5)
+    case 3: if (x < 1.0e-5 && x > -1.0e-5)
                 return (0.5)*(0.5-y)*(0.5-z);
-            if (x < 1.0+1e-5 && x > 1.0-1e-5)
+            if (x < 1.0+1.0e-5 && x > 1.0-1.0e-5)
                 return (-0.5)*(0.5-y)*(0.5-z);
-            if (y < 1.0e-5 && y > -1e-5)
+            if (y < 1.0e-5 && y > -1.0e-5)
                 return (0.5-x)*(0.5)*(0.5-z);
-            if (y < 1.0+1e-5 && y > 1.0-1e-5)
+            if (y < 1.0+1.0e-5 && y > 1.0-1.0e-5)
                 return (0.5-x)*(-0.5)*(0.5-z);
-            if (z < 1.0e-5 && z > -1e-5)
+            if (z < 1.0e-5 && z > -1.0e-5)
                 return (0.5-x)*(0.5-y)*(0.5);
-            if (z < 1.0+1e-5 && z > 1.0-1e-5)
+            if (z < 1.0+1.0e-5 && z > 1.0-1.0e-5)
                 return (0.5-x)*(0.5-y)*(-0.5);
             assert(false); // nicht vorgesehen
-    case 4: if (x < 1.0e-5 && x > -1e-5)
+    case 4: if (x < 1.0e-5 && x > -1.0e-5)
                 return -(0.5-y)*(0.5-z);
-            if (x < 1.0+1e-5 && x > 1.0-1e-5)
+            if (x < 1.0+1.0e-5 && x > 1.0-1.0e-5)
                 return -(0.5-y)*(0.5-z);
-            if (y < 1.0e-5 && y > -1e-5)
+            if (y < 1.0e-5 && y > -1.0e-5)
                 return -(0.5-x)*(0.5-z);
-            if (y < 1.0+1e-5 && y > 1.0-1e-5)
+            if (y < 1.0+1.0e-5 && y > 1.0-1.0e-5)
                 return -(0.5-x)*(0.5-z);
-            if (z < 1.0e-5 && z > -1e-5)
+            if (z < 1.0e-5 && z > -1.0e-5)
                 return -(0.5-x)*(0.5-y);
-            if (z < 1.0+1e-5 && z > 1.0-1e-5)
+            if (z < 1.0+1.0e-5 && z > 1.0-1.0e-5)
                 return -(0.5-x)*(0.5-y);
             assert(false); // nicht vorgesehen
-//    case 4: if (x < 1.0e-5 && x > -1e-5)
-//                return 0.5 - 0.5*(y+z) + y*z;
-//            if (x < 1.0+1e-5 && x > 1.0-1e-5)
-//                return - 0.5 + 0.5*(y+z) - y*z;
-//            if (y < 1.0e-5 && y > -1e-5)
-//                return 0.5 - 0.5*(x+z) + x*z;
-//            if (y < 1.0+1e-5 && y > 1.0-1e-5)
-//                return - 0.5 + 0.5*(x+z) - x*z;
-//            if (z < 1.0e-5 && z > -1e-5)
-//                return 0.5 - 0.5*(y+x) + y*x;
-//            if (z < 1.0+1e-5 && z > 1.0-1e-5)
-//                return - 0.5 + 0.5*(y+x) - y*x;
-//            assert(false); // nicht vorgesehen
     // 5: u, 6: f, 7: dirichlet, 8: neumann
     case 5: return x*(1.0-x)*y*(1.0-y)*z*(1.0-z);
     case 6: return 2.0*(x*(1.0-x)*y*(1.0-y)+x*(1.0-x)*z*(1.0-z)+y*(1.0-y)*z*(1.0-z));
     case 7: return 0.0;
-    case 8: assert(false); return 0.0; //TODO
+    case 8: if (x < 1.0e-5 && x > -1.0e-5)
+                return (1.0-2.0*x)*y*(1.0-y)*z*(1.0-z);
+            if (x < 1.0+1.0e-5 && x > 1.0-1.0e-5)
+                return (1.0-2.0*x)*y*(1.0-y)*z*(1.0-z);
+            if (y < 1.0e-5 && y > -1.0e-5)
+                return x*(1.0-x)*(1.0-2.0*y)*z*(1.0-z);
+            if (y < 1.0+1.0e-5 && y > 1.0-1.0e-5)
+                return x*(1.0-x)*(1.0-2.0*y)*z*(1.0-z);
+            if (z < 1.0e-5 && z > -1.0e-5)
+                return x*(1.0-x)*y*(1.0-y)*(1.0-2.0*z);
+            if (z < 1.0+1.0e-5 && z > 1.0-1.0e-5)
+                return x*(1.0-x)*y*(1.0-y)*(1.0-2.0*z);
+            assert(false); // nicht vorgesehen
     }
     assert(!true); // nicht vorgesehen
     return 0.0;
