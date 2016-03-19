cb0f424c684cba3576c55f80726c5c846598d6c9
heikop <heiko.poelstra@tu-dortmund.de>
2016-03-19 12:05:31 +0100

    avoid many quad points calculations

--- Projekt/src/assemblyRHSNeumann.cpp
+++ Projekt/src/assemblyRHSNeumann.cpp
@@ -5,10 +5,13 @@ namespace Icarus
 
 double assembleFem::assemblyRHSNeumann(int Ebene, int leftright, mathfunction g)
 {
+    assert(Ebene == 1 || Ebene == 2 || Ebene == 3);
+
     int n(_e.size());
     double RHS(0.0);
 
-    std::vector<double> R1(9), R2(9), Basis2d(9);
+    //std::vector<double> R1(9), R2(9);
+    std::vector<double> Basis2d(9);
 
     //X-Y-Ebene
     if(Ebene == 1)
@@ -16,17 +19,17 @@ double assembleFem::assemblyRHSNeumann(int Ebene, int leftright, mathfunction g)
         for(int i(0); i<n; i++)
         {
             //Quadraturpunkte fuer X-Y-Ebene
-            R1 = get_quadrature_xpoints_2d_1();
-            R2 = get_quadrature_ypoints_2d_1();
+            //R1 = get_quadrature_xpoints_2d_1();
+            //R2 = get_quadrature_ypoints_2d_1();
             double e_x(getx(_e[i]));
             double e_y(gety(_e[i]));
             double e_z(getz(_e[i]) + _h*static_cast<double>(leftright));
             Basis2d = evaluated_Basis2d_1(_A[i] - 4*leftright);
 
-            int nqp(R1.size());
-            for(int q(0); q<nqp; q++)
+            for(int q(0); q < 9; q++)
                 //Zum auswerten von g, translatiere die Gauspunkte zum Element e[i]
-                RHS += Basis2d[q] * g.eval(R1[q] + e_x, R2[q] + e_y, e_z) * _weight_2d[q];
+                //RHS += Basis2d[q] * g.eval(R1[q] + e_x, R2[q] + e_y, e_z) * _weight_2d[q];
+                RHS += Basis2d[q] * g.eval(_quadpoints_2d_1[q] + e_x, _quadpoints_2d_2[q] + e_y, e_z) * _weight_2d[q];
         }
     }
     //X-Z-Ebene
@@ -35,17 +38,17 @@ double assembleFem::assemblyRHSNeumann(int Ebene, int leftright, mathfunction g)
         for(int i(0); i<n; i++)
         {
             //Quadraturpunkte fuer X-Z-Ebene
-            R1 = get_quadrature_xpoints_2d_2();
-            R2 = get_quadrature_zpoints_2d_2();
+            //R1 = get_quadrature_xpoints_2d_2();
+            //R2 = get_quadrature_zpoints_2d_2();
             double e_x(getx(_e[i]));
             double e_y(gety(_e[i]) + _h*static_cast<double>(leftright));
             double e_z(getz(_e[i]));
             Basis2d = evaluated_Basis2d_2(_A[i] - 2*leftright);
 
-            int nqp(R1.size());
-            for(int q(0); q<nqp; q++)
+            for(int q(0); q < 9; q++)
                 //Zum auswerten von g, translatiere die Gauspunkte zum Element e[i]
-                RHS += Basis2d[q] * g.eval(R1[q] + e_x, e_y, R2[q] + e_z) * _weight_2d[q];
+                //RHS += Basis2d[q] * g.eval(R1[q] + e_x, e_y, R2[q] + e_z) * _weight_2d[q];
+                RHS += Basis2d[q] * g.eval(_quadpoints_2d_1[q] + e_x, e_y, _quadpoints_2d_2[q] + e_z) * _weight_2d[q];
         }
     }
     //Y-Z-Ebene
@@ -54,20 +57,20 @@ double assembleFem::assemblyRHSNeumann(int Ebene, int leftright, mathfunction g)
         for(int i(0); i<n; i++)
         {
             //Quadraturpunkte fuer Y-Z-Ebene
-            R1 = get_quadrature_ypoints_2d_3();
-            R2 = get_quadrature_zpoints_2d_3();
+            //R1 = get_quadrature_ypoints_2d_3();
+            //R2 = get_quadrature_zpoints_2d_3();
             double e_x(getx(_e[i]) + _h*static_cast<double>(leftright));
             double e_y(gety(_e[i]));
             double e_z(getz(_e[i]));
             Basis2d = evaluated_Basis2d_3(_A[i] - leftright);
 
-            int nqp(R1.size());
-            for(int q(0); q<nqp; q++)
+            for(int q(0); q < 9; q++)
                 //Zum auswerten von g, translatiere die Gauspunkte zum Element e[i]
-                RHS += Basis2d[q] * g.eval(e_x, R1[q] + e_y, R2[q] + e_z) * _weight_2d[q];
+                //RHS += Basis2d[q] * g.eval(e_x, R1[q] + e_y, R2[q] + e_z) * _weight_2d[q];
+                RHS += Basis2d[q] * g.eval(e_x, _quadpoints_2d_1[q] + e_y, _quadpoints_2d_2[q] + e_z) * _weight_2d[q];
         }
     }
-    assert(Ebene == 1 || Ebene == 2 || Ebene ==3);
+
     return RHS;
 }//assemblyRHSNeumann
 
